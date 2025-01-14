de6d0a0b7fbb73de008eb420936a3fd712232003
Abdul Bayees <bayees1@gmail.com>
2025-01-15 04:36:11 +0530

    building app

--- middleware.tsx
+++ middleware.tsx
@@ -1,19 +1,19 @@
-// app/middleware.js
-import { NextRequest, NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
+// // app/middleware.js
+// import { NextRequest, NextResponse } from "next/server";
+// import { getToken } from "next-auth/jwt";
 
-export async function middleware(request:NextRequest) {
-  const token = await getToken({ req: request });
+// export async function middleware(request:NextRequest) {
+//   const token = await getToken({ req: request });
 
-  // Redirect to home if not authenticated and accessing a protected route
-  if (!token && request.nextUrl.pathname === '/Dashboard') {
-    return NextResponse.redirect(new URL('/auth/signin', request.url));
-  }
-  if (!token && request.nextUrl.pathname === '/Dashboard/Workspace') {
-    return NextResponse.redirect(new URL('/auth/signin', request.url));
-  }
-}
+//   // Redirect to home if not authenticated and accessing a protected route
+//   if (!token && request.nextUrl.pathname === '/Dashboard') {
+//     return NextResponse.redirect(new URL('/auth/signin', request.url));
+//   }
+//   if (!token && request.nextUrl.pathname === '/Dashboard/Workspace') {
+//     return NextResponse.redirect(new URL('/auth/signin', request.url));
+//   }
+// }
 
-export const config = {
-  matcher: ['/Dashboard/:path*'], // Apply middleware to the Dashboard route
-};
+// export const config = {
+//   matcher: ['/Dashboard/:path*'], // Apply middleware to the Dashboard route
+// };
