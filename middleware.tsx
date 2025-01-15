742ef5dce100892094710093583ecb3896d8d4b9
Abdul Bayees <bayees1@gmail.com>
2025-01-15 15:46:50 +0530

    tryiing middleware

--- middleware.tsx
+++ middleware.tsx
@@ -1,15 +1,14 @@
-// app/middleware.js
+// app/middleware.tsx
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-export async function middleware(request:NextRequest) {
+export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
+  console.log('token', token);
 
   // Redirect to home if not authenticated and accessing a protected route
-  if (!token && request.nextUrl.pathname === '/Dashboard') {
-    return NextResponse.redirect(new URL('/auth/signin', request.url));
-  }
-  if (!token && request.nextUrl.pathname === '/Dashboard/Workspace') {
+  if (!token && request.nextUrl.pathname.startsWith('/Dashboard')) {
+    console.log('redirecting to signin', request.url, token);
     return NextResponse.redirect(new URL('/auth/signin', request.url));
   }
 }
